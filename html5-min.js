ea19843de1e35b671ad5cfe2ff859b74d1c65d30
Niclas Gelin <info@niclasgelin.se>
2012-05-22 11:18:36 +0200

    Update minified source

--- html5-min.js
+++ html5-min.js
@@ -1,2 +1,2 @@
-/*! HTML5 Enabling Script v1.0.2 - https://github.com/niclasgelin/html5-js */
-var html5=(function(){var c=["header","hgroup","footer","article","section","aside","nav","figure","figcaption","embed","audio","video","canvas","details","summary","time","mark"],a=document.createDocumentFragment();for(var b=0;b<c.length;b++){a.appendChild(document.createElement(c[b]))}}());
+/*! HTML5 Enabling Script v1.0.4 - https://github.com/niclasgelin/html5-js */
+var html5=(function(){var c=["header","hgroup","footer","article","section","aside","nav","figure","figcaption","embed","audio","video","canvas","details","summary","time","mark"],a=document.createDocumentFragment();for(var b in c){a.appendChild(document.createElement(c[b]))}}());
