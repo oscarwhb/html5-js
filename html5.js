53e7b2affbeb637cabd825ed309ab3fd17cd0c0f
Niclas Gelin <info@niclasgelin.se>
2012-05-22 11:56:55 +0200

    Self-invoked anonymous function
    And update version

--- html5.js
+++ html5.js
@@ -1,4 +1,4 @@
-/*! HTML5 Enabling Script v1.0.4 - https://github.com/niclasgelin/html5-js */
+/*! HTML5 Enabling Script v1.0.5 - https://github.com/niclasgelin/html5-js */
 /*
  * A minimal HTML5 elements shim.
  *
@@ -6,10 +6,10 @@
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to: http://unlicense.org
  */
-var html5 = (function() {
+(function() {
   var elements = ["header","hgroup","footer","article","section","aside","nav","figure","figcaption","embed","audio","video","canvas","details","summary","time","mark"],
       fragment = document.createDocumentFragment();
   for (var i in elements) {
     fragment.appendChild(document.createElement(elements[i]));
   }
-}());
+})();
