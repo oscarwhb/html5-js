74b387ea1821378945c407f686121e716b65ebb6
niclasgelin <info@niclasgelin.se>
2012-12-19 20:02:22 +0100

    Add protective semi-colon

--- html5.js
+++ html5.js
@@ -5,7 +5,7 @@
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to: http://unlicense.org
  */
-(function() {
+;(function() {
   var elements = ["header","hgroup","footer","article","section","aside","nav","figure","figcaption","embed","audio","video","canvas","details","summary","time","mark"];
   for (var i in elements) {
     document.createElement(elements[i])
