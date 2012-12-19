a23ae99d90b340572eaa958280a682c2bf0fbbb8
niclasgelin <info@niclasgelin.se>
2012-12-19 20:41:49 +0100

    Use strict statement
    New lines improve readability

--- html5.js
+++ html5.js
@@ -1,4 +1,4 @@
-/*! HTML5 Enabling Script v1.0.8 - https://github.com/niclasgelin/html5-js */
+/*! HTML5 Enabling Script v1.0.9 - https://github.com/niclasgelin/html5-js */
 /*
  * A minimal HTML5 elements shim.
  *
@@ -6,9 +6,13 @@
  * For more information, please refer to: http://unlicense.org
  */
 ;(function() {
+  "use strict";
+
   var elements = ["header","hgroup","footer","article","section","aside","nav","figure","figcaption","embed","audio","video","canvas","details","summary","time","mark"],
       i = elements.length;
+
   while (i--) {
     document.createElement(elements[i]);
   }
+
 }());
