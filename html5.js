7a595b96d74098717381747b1627f5131c5810b1
niclasgelin <info@niclasgelin.se>
2012-12-19 20:09:02 +0100

    Use simpler loop pattern

--- html5.js
+++ html5.js
@@ -1,4 +1,4 @@
-/*! HTML5 Enabling Script v1.0.6 - https://github.com/niclasgelin/html5-js */
+/*! HTML5 Enabling Script v1.0.7 - https://github.com/niclasgelin/html5-js */
 /*
  * A minimal HTML5 elements shim.
  *
@@ -6,8 +6,9 @@
  * For more information, please refer to: http://unlicense.org
  */
 ;(function() {
-  var elements = ["header","hgroup","footer","article","section","aside","nav","figure","figcaption","embed","audio","video","canvas","details","summary","time","mark"];
-  for (var i in elements) {
-    document.createElement(elements[i])
+  var elements = ["header","hgroup","footer","article","section","aside","nav","figure","figcaption","embed","audio","video","canvas","details","summary","time","mark"],
+      i = elements.length;
+  while (i--) {
+    document.createElement(elements[i]);
   }
 })();
